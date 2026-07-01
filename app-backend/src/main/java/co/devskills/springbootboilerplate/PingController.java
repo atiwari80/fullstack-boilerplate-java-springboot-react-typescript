a4752ca7283745288b2a3cba4aa4b8f4010a9bcd
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-01 13:13:42 -0400

    fix: remediate vulnerability in app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java

--- app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java
+++ app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java
@@ -1,7 +1,4 @@
-String query = "SELECT * FROM users WHERE name = ?";
-java.sql.Connection connection = 
-druid.Connection.getConnection();
-java.sql.PreparedStatement statement = connection.prepareStatement(query);
+java.sql.PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE name = ?");
 statement.setString(1, name);
 statement.execute();
 connection.commit();

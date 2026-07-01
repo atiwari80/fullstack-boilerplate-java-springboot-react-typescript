504a90bc0077ae379fce8c7968e59d19387b01da
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-01 10:54:55 -0400

    fix: remediate vulnerability in app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java

--- app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java
+++ app-backend/src/main/java/co/devskills/springbootboilerplate/PingController.java
@@ -1,4 +1,4 @@
-String query = "SELECT * FROM users WHERE name = ?\n";
-try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sqlitestorage.db")) {\n    PreparedStatement statement = connection.prepareStatement(query);
-    statement.setString(1, name);
-    statement.execute();\n}
+1: String query = "SELECT * FROM users WHERE name = ?\n";
+2: try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sqlitestorage.db")) {\n    PreparedStatement statement = connection.prepareStatement(query);
+3:     statement.setString(1, name);
+4:     statement.execute();\n}
